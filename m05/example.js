d1544890b7477e72fa5f0fcc5d2b43963ddbffd4
nkijes24 <166266806+nkijes24@users.noreply.github.com>
2024-05-04 09:04:48 -0700

    Update example.js

--- m05/example.js
+++ m05/example.js
@@ -1,18 +1,18 @@
-var table = 5;             // Unit of table
-var operator = 'addition'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var table = 5;             								// Unit of table
+var operator = 'multiplication'; 						// Type of calculation
+var i = 1;                 								// Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';              // Message
 
-if (operator === 'addition') {
-  // Do addition
+if (operator === 'multiplication') {
+  // Do multiplication
   while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    msg += i + ' * ' + table + ' = ' + (i + table) + '<br />';
     i++;
   }
 } else {
   // Do multiplication
   while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    msg += i + ' * ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
 }
