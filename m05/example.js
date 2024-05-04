101801a44ae9ecbf8f33d2e16431ed080a0a87bc
nkijes24 <166266806+nkijes24@users.noreply.github.com>
2024-05-04 09:22:50 -0700

    Update example.js

--- m05/example.js
+++ m05/example.js
@@ -6,12 +6,6 @@ var msg = '<h2>Multiplication Table</h2>';              // Message
 if (operator === 'multiplication') {
   // Do multiplication
   while (i < 11) {
-    msg += i + ' * ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
     msg += i + ' * ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
