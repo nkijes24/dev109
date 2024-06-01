f27716bccb3664a25f8ef037f252d00e7674f857
nkijes24 <166266806+nkijes24@users.noreply.github.com>
2024-06-01 15:03:45 -0700

    Create m09.js

---
+++ m09/m09.js
@@ -0,0 +1,12 @@
+ window.addEventListener("click", event => {
+    let dot = document.createElement("div");
+    dot.className = "dot";
+    dot.style.left = (event.pageX - 4) + "px";
+    dot.style.top = (event.pageY - 4) + "px";
+    document.body.appendChild(dot);
+  });
+toolbar.addEventListener('click', e => {
+    if (e.target.id === 'clear') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
